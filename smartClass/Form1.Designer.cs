c44f9be1d403a7df754bf800c84e7d0696a5a912
wangxian <xian.wang@ronds.com.cn>
2020-09-21 11:07:10 +0800

    首次提交

---
+++ smartClass/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace smartClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.importExcelBtn = new System.Windows.Forms.Button();
+            this.importExcelData = new System.Windows.Forms.DataGridView();
+            this.exportBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.className = new System.Windows.Forms.TextBox();
+            this.dateTime = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.importExcelData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // importExcelBtn
+            // 
+            this.importExcelBtn.Location = new System.Drawing.Point(12, 12);
+            this.importExcelBtn.Name = "importExcelBtn";
+            this.importExcelBtn.Size = new System.Drawing.Size(84, 35);
+            this.importExcelBtn.TabIndex = 0;
+            this.importExcelBtn.Text = "导入课程表";
+            this.importExcelBtn.UseVisualStyleBackColor = true;
+            this.importExcelBtn.Click += new System.EventHandler(this.importExcelBtn_Click);
+            // 
+            // importExcelData
+            // 
+            this.importExcelData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.importExcelData.Location = new System.Drawing.Point(12, 53);
+            this.importExcelData.Name = "importExcelData";
+            this.importExcelData.RowTemplate.Height = 23;
+            this.importExcelData.Size = new System.Drawing.Size(776, 302);
+            this.importExcelData.TabIndex = 1;
+            // 
+            // exportBtn
+            // 
+            this.exportBtn.Location = new System.Drawing.Point(695, 12);
+            this.exportBtn.Name = "exportBtn";
+            this.exportBtn.Size = new System.Drawing.Size(93, 37);
+            this.exportBtn.TabIndex = 2;
+            this.exportBtn.Text = "导出";
+            this.exportBtn.UseVisualStyleBackColor = true;
+            this.exportBtn.Click += new System.EventHandler(this.exportBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "班级：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(345, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "日期：";
+            // 
+            // className
+            // 
+            this.className.Location = new System.Drawing.Point(171, 20);
+            this.className.Name = "className";
+            this.className.Size = new System.Drawing.Size(131, 21);
+            this.className.TabIndex = 5;
+            // 
+            // dateTime
+            // 
+            this.dateTime.CustomFormat = "yyyy-MM-dd";
+            this.dateTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTime.Location = new System.Drawing.Point(392, 20);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.Size = new System.Drawing.Size(200, 21);
+            this.dateTime.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("方正舒体", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(636, 372);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "由「一根网线」制作";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 398);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTime);
+            this.Controls.Add(this.className);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exportBtn);
+            this.Controls.Add(this.importExcelData);
+            this.Controls.Add(this.importExcelBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "电子班牌助手";
+            ((System.ComponentModel.ISupportInitialize)(this.importExcelData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button importExcelBtn;
+        private System.Windows.Forms.DataGridView importExcelData;
+        private System.Windows.Forms.Button exportBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox className;
+        private System.Windows.Forms.DateTimePicker dateTime;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ smartClass/Form1.cs
@@ -0,0 +1,167 @@
+﻿using smartClass.Properties;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace smartClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   
+        private void importExcelBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            ofd.Filter = "工作表|*.xls";
+            string filename = ofd.FileName;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //连接字符串
+                string constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filename + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1;'";
+                //创建一个数据链接
+                OleDbConnection myConn = new OleDbConnection(constr);
+                string strCom = " SELECT * FROM [Sheet1$] ";
+                myConn.Open();
+
+                //打开数据链接，得到一个数据集
+                OleDbDataAdapter da = new OleDbDataAdapter(strCom, myConn);
+                // DataSet对象
+                DataSet ds = new DataSet();
+                //得到自己的DataSet对象
+                da.Fill(ds, "[Sheet1$]");
+                //关闭此数据链接
+                myConn.Close();
+                this.importExcelData.DataSource = ds.Tables[0];
+            }
+        }
+
+        private void exportBtn_Click(object sender, EventArgs e)
+        {
+            if(this.className.Text == "")
+            {
+                MessageBox.Show("请填写所在班级！");
+                return;
+            }
+            if (ExportDataGridview(this.importExcelData, true))
+                MessageBox.Show("导出成功，请记得保存!");
+            else
+                MessageBox.Show("导出未成功，请检查是否有错!");
+        }
+
+        public bool ExportDataGridview(DataGridView gridView, bool isShowExcle)//生成Excel
+        {
+            if (gridView.Rows.Count == 0)
+            {
+                MessageBox.Show("未导入原始课表数据");
+         
+                return false;
+            }
+            //建立Excel对象
+            Excel.Application excel = new Excel.Application();
+            excel.Application.Workbooks.Add(true);
+            excel.Visible = isShowExcle;
+            //教室 日期  节次 学科  任课教师工号 任课教师姓名
+            string[] herder = new string[6] { "教室", "日期", "节次", "学科", "任课教师工号", "任课教师姓名"};
+            //生成字段名称
+            for (int i = 0; i < herder.Length; i++)
+            {
+                excel.Cells[1, i + 1] = herder[i];
+            }
+            //填充数据
+
+            //21 22 23
+
+            ArrayList rowList = new ArrayList();
+
+            int idx = 1;
+            for (int i = 2; i < gridView.ColumnCount; i++)
+            {
+                //处理数据              
+                for (int j = 2; j < gridView.RowCount - 1; j++)
+                {
+
+                    string key = gridView[i, j].Value.ToString();
+
+                    string _key = key.Replace("（", "(").Replace("）", ")");
+
+                    string teacher = Regex.Replace(_key, @"(.*\()(.*)(\).*)", "$2"); ;
+
+
+
+                    string course = _key.Replace("(" + teacher +")","");
+
+
+                    DateTime dt = this.dateTime.Value;
+                    if (6< idx && idx<=12)
+                    {
+                        dt = dt.AddDays(1);
+                    }
+                    else if (12 < idx && idx <= 18)
+                    {
+                        dt = dt.AddDays(2);
+
+                    }
+                    else if (18 < idx && idx <= 24)
+                    {
+                        dt = dt.AddDays(3);
+
+                    }
+                    else if (24 < idx && idx <= 30)
+                    {
+                        dt = dt.AddDays(4);
+
+                    }
+                    else if (30 < idx && idx <= 36)
+                    {
+                        dt = dt.AddDays(5);
+
+                    }
+                    idx++;
+
+
+
+
+                    string[] _row = new string[6] 
+                    {
+                        this.className.Text,
+                        dt.ToShortDateString(),
+                        gridView[1, j].Value.ToString().Replace('/','-'),
+                        course,
+                        Resources.ResourceManager.GetString(teacher),
+                        teacher
+                    };        
+                    rowList.Add(_row);               
+                }
+           
+            }
+                       
+            for (int m=0;m< rowList.Count;m++)
+            {
+                string[] _row = (string[])rowList[m];
+
+                for (int n = 0; n< _row.Length;n++)
+                {
+                    excel.Cells[m + 2, n + 1] = _row[n];                  
+                }
+            }
+            return true;
+        }
+
+    }
+}
